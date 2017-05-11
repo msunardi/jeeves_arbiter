b748f1083c533a7f2f4d57cbb8f60a6e39740009
Rick Armstrong <richarda@pdx.edu>
2017-05-11 05:10:48 -0700

    Add description argument

--- src/arbiter_client.py
+++ src/arbiter_client.py
@@ -4,25 +4,26 @@ import sys
 import rospy
 from arbiter.srv import *
 
-def arbiter_client(req_itype, req_value):
+def arbiter_client(req_itype, req_value, req_description):
     rospy.wait_for_service('arbiter_server')
     try:
         foo = rospy.ServiceProxy('arbiter_server', Interact)
-        resp = foo(req_itype, req_value)
+        resp = foo(req_itype, req_value, req_description)
         return resp.response
     except rospy.ServiceException, e:
         print "Service call failed: %s" % e
         rospy.logerr("Service call failed: %s" % e)
 
 def usage():
-    return "%s [type] [value]" % sys.argv[0]
+    return "%s [type] [value] [description]" % sys.argv[0]
 
 if __name__ == "__main__":
-    if len(sys.argv) == 3:
+    if len(sys.argv) == 4:
         itype = str(sys.argv[1])
         value = str(sys.argv[2])
+        description = str(sys.argv[3])
     else:
         print usage()
         sys.exit(1)
-    print "Requesting {Type: %s, Value: %s}" % (itype, value)
-    print "Response: %s" % arbiter_client(itype, value)
+    print "Requesting {Type: %s, Value: %s, Description: %s}" % (itype, value, description)
+    print "Response: %s" % arbiter_client(itype, value, description)
