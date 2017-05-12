5362c6b8730b5e212a8ae47687d26b75a062b9bf
Mathias Sunardi <misunardi@hotmail.com>
2017-05-12 00:01:31 -0700

    Convert to class

--- src/nav_spiel.py
+++ src/nav_spiel.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 import sys, os, time, json, string
+import threading
+
 import rospy
 import aiml                               # Artificial Intelligence Markup Language
 from std_msgs.msg import String
@@ -9,177 +11,196 @@ import time
 import subprocess
 from actionlib_msgs.msg import GoalStatusArray
 
-# Set current working directory
-cwd = os.chdir(os.path.dirname(os.path.realpath(__file__)) + '/../');
-cwd = os.getcwd() + '/';
+class NavSpiel(threading.Thread):
 
-jeeves = aiml.Kernel()
-jeeves.learn(cwd + "jeeves.xml")
-jeeves.respond('load aiml b') #finished initializing
+    def __init__(self):
+        # Set current working directory
+        cwd = os.chdir(os.path.dirname(os.path.realpath(__file__)) + '/../');
+        cwd = os.getcwd() + '/';
 
-locations = {'ee': 'EE FISHBOWL', \
-             'NEAR_lab': 'NEAR LAB', \
-             'near_lab': 'NEAR LAB', \
-             'robot_lab': 'ROBOTICS LAB', \
-             'Biomedical_lab': 'BIOMEDICAL SIGNAL LAB', \
-             'biomedical_lab': 'BIOMEDICAL SIGNAL LAB', \
-             'stairs': 'EB STAIRS', \
-             'portland_state': 'PORTLAND STATE', \
-             'portland_state_university': 'PORTLAND STATE'}
+        self.jeeves = aiml.Kernel()
+        self.jeeves.learn(cwd + "jeeves.xml")
+        self.jeeves.respond('load aiml b') #finished initializing
 
-facts_or_jokes = ['FACTS', 'JOKES']
+        self.locations = {'ee': 'EE FISHBOWL', \
+                    'NEAR_lab': 'NEAR LAB', \
+                    'near_lab': 'NEAR LAB', \
+                    'robot_lab': 'ROBOTICS LAB', \
+                    'Biomedical_lab': 'BIOMEDICAL SIGNAL LAB', \
+                    'biomedical_lab': 'BIOMEDICAL SIGNAL LAB', \
+                    'stairs': 'EB STAIRS', \
+                    'portland_state': 'PORTLAND STATE', \
+                    'portland_state_university': 'PORTLAND STATE'}
 
-sub = rospy.Subscriber('chatter', String, queue_size=10)
-say = rospy.Publisher('/jeeves_speech/speech_synthesis', String, queue_size=10)
+        self.facts_or_jokes = ['FACTS', 'JOKES']
+        self.comment = {'FACTS': ['By the way, did you know . . .', 'It reminds me . . .', 'Here is a little trivia . . .'],
+                'JOKES': ['Funny story ...', 'This is a true story ...'],
+                'CLOSING': {'FACTS': ['. . . Interesting, isn\'t it?', '. . . Fascinating', '. . . Remarkable', 'Fascinating, no?', ''],
+                            'JOKES': ['. . Ha ha ha', 'Get it? . . Heh Heh.', '. . . Heh heh heh.', ''],
+                            'END': ['Thank you for your attention.', 'I hope you enjoyed it.', 'Let me know if there is anything else I can do for you.']}}
 
-spiel = True
-place = ''
-arrived = False
-speaking = False
-stop_all = False
+        self.sub = rospy.Subscriber('chatter', String, queue_size=10)
+        self.say = rospy.Publisher('/jeeves_speech/speech_synthesis', String, queue_size=10)
 
-collect = 10
+        self.spiel = True
+        self.arrived = False
+        self.speaking = False
+        self.stop_all = False
 
-def callback(message):
-    global place, spiel, arrived, stop_all
-    # topic = "PORTLAND STATE"
-    # topic = "INTEL LAB"
-    topic = locations[message.data]
-    place = topic
-    count = 5
-    arrived = False
-    rcvd = []   # Container for spiel
+        self.collect = 4
+        self.place = ''
 
-    jk_prob = 0.05
-    jk_prob = 0.2
-    comment = {'FACTS': ['By the way, did you know . . .', 'It reminds me . . .', 'Here is a little trivia . . .'],
-            'JOKES': ['Funny story ...', 'This is a true story ...'],
-            'CLOSING': {'FACTS': ['. . . Interesting, isn\'t it?', '. . . Fascinating', '. . . Remarkable', 'Fascinating, no?', ''],
-                        'JOKES': ['. . Ha ha ha', 'Get it? . . Heh Heh.', '. . . Heh heh heh.', ''],
-                        'END': ['Thank you for your attention.', 'I hope you enjoyed it.', 'Let me know if there is anything else I can do for you.']}}
-    fj_count = 0
-    for i in range(count):
-        rsp = jeeves.respond(topic)
+        rospy.Subscriber("/spiel", String, self.callback)
+        rospy.Subscriber("/done", String, self.done)
+        rospy.Subscriber("/goal_reached", String, self.goal_reached)
+        rospy.Subscriber("/move_base/status", GoalStatusArray, self.goal_reached_action)
 
-        # If don't know, abort
-        if not rsp:
-            rcvd.append('I am sorry, but I do not know much about %s yet. ' % topic)
-            break
+        threading.Thread.__init__(self)
+        self.sleeper = rospy.Rate(10)
+
+    def callback(self, message):
+
+        # topic = "PORTLAND STATE"
+        # topic = "INTEL LAB"
+        topic = self.locations[message.data]
+        self.place = topic
+        self.arrived = False
+        rcvd = []   # Container for spiel
+
+        # Probability of inserting a joke/fact
+        jk_prob = 0.05
+        jk_prob = 0.2
         
-        # Collect info
-        if rsp not in rcvd:
-            rcvd.append(rsp)
-            print rsp
-            
-        # Insert random joke/fact
-        ran = r.random()
-        if ran < jk_prob:
-            factjoke = r.choice(facts_or_jokes)
-            rx = jeeves.respond(factjoke)
-            if rx not in rcvd:
-                rx = r.choice(comment[factjoke]) + '. ' + rx + '. ' + r.choice(comment['CLOSING'][factjoke])
-                rcvd.append(rx)
-                print rx
-    rcvd.append('And that is all I can say about %s %s. %s' % (topic, r.choice(['for now', '']), r.choice(comment['CLOSING']['END'])))
-        
-    for msg in rcvd:
-        if not spiel:
-            spiel = True
-            msg = "OK. I'll stop."
-            festival(msg)
-            break
-        try:
-            while speaking:
-                pass
-            festival(msg)
-        except Exception as e:
-            print e
-        finally:
-            sl = r.random()*0.5
-            print "I'm done - sleeping for %s" % sl        
-            time.sleep(sl)
-    else:
-        spiel = False
-    print "Arrived: %s" % arrived
-    while not arrived and not spiel:
-        try:
-            if arrived or stop_all:
-                spiel = True
-                msg = "OK. I'll stop."
-                festival(msg)
-                stop_all = False
+        fj_count = 0
+        for i in range(self.collect):
+            rsp = self.jeeves.respond(topic)
+
+            # If don't know, abort
+            if not rsp:
+                rcvd.append('I am sorry, but I do not know much about %s yet. ' % topic)
                 break
-            factjoke = r.choice(facts_or_jokes)
-            rx = jeeves.respond(factjoke)
-            rx = r.choice(comment[factjoke]) + '. ' + rx + '. ' + r.choice(comment['CLOSING'][factjoke])
-            
-            while speaking and not arrived:
+
+            # Collect info
+            if rsp not in rcvd:
+                rcvd.append(rsp)
+                print rsp
+
+            # Insert random joke/fact
+            ran = r.random()
+            if ran < jk_prob:
+                factjoke = r.choice(self.facts_or_jokes)
+                rx = self.jeeves.respond(factjoke)
+                if rx not in rcvd:
+                    rx = r.choice(self.comment[factjoke]) + '. ' + rx + '. ' + r.choice(self.comment['CLOSING'][factjoke])
+                    rcvd.append(rx)
+                    print rx
+        rcvd.append('%s that is all I can say about %s %s. %s' % (r.choice(['And', 'Well, ', '']), topic, r.choice(['for now', '']), r.choice(self.comment['CLOSING']['END'])))
+
+        for msg in rcvd:
+            if not self.spiel:
+                self.spiel = True
+                msg = "OK. I'll stop."
+                self.festival(msg)
+                break
+            try:
+                while self.speaking:
+                    pass
+                self.festival(msg)
+            except Exception as e:
+                print e
+            finally:
+                sl = r.random()*0.5
+                print "I'm done - sleeping for %s" % sl
+                time.sleep(sl)
+        else:
+            self.spiel = False
+        print "After spiel - Arrived: %s" % self.arrived
+        while not self.arrived and not self.spiel:
+            try:
+                if self.arrived or self.stop_all:
+                    self.spiel = True
+                    msg = "OK. I'll stop."
+                    self.festival(msg)
+                    self.stop_all = False
+                    break
+                factjoke = r.choice(self.facts_or_jokes)
+                rx = self.jeeves.respond(factjoke)
+                rx = r.choice(self.comment[factjoke]) + '. ' + rx + '. ' + r.choice(self.comment['CLOSING'][factjoke])
+
+                while self.speaking and not self.arrived:
+                    pass
+                if self.arrived and r.random() < 0.3:
+                    break
+                self.festival(rx)
+            except Exception as e:
+                print e
+            finally:
+                sl = r.random()*3.0
+                print "Not arrived yet - sleeping for %s" % sl
+                time.sleep(sl)
+        self.arrived = False
+        self.spiel = True
+        self.stop_all = False
+
+    def done(self, message):
+        if message.data == "no":
+            self.spiel = True
+            self.stop_all = False
+        else:
+            self.spiel = False
+            self.stop_all = True
+
+        self.arrived = False
+
+    def goal_reached(self, message):
+        print "Arrived: %s" % self.arrived
+        if 'Goal reached' in message.data and not self.arrived:
+            arrive = ['We have reached %s.', 'Well, here we are . . . %s', 'Welcome to the %s . . . ', 'This is the %s . . . ']
+            msg = r.choice(arrive) % self.place
+            while self.speaking or self.spiel:
                 pass
-            if arrived and r.random() < 0.3:
-                break      
-            festival(rx)
+            self.festival(msg)
+            self.arrived = True
+            print "Yes Arrived: %s" % self.arrived
+
+    def goal_reached_action(self, message):
+        print "True Arrived: %s" % self.arrived
+        if 'Goal reached' in message.status_list[-1].text and not self.arrived:
+            arrive = ['We have reached %s.', 'Well, here we are . . . %s', 'Welcome to the %s . . . ', 'This is the %s . . . ']
+            msg = r.choice(arrive) % self.place
+            while self.speaking or self.spiel:
+                pass
+            self.festival(msg)
+            self.arrived = True
+            print "Yes Arrived: %s" % self.arrived
+
+    def festival(self, say):
+        self.speaking = True
+        try:
+            subprocess.call(["festival", "--batch", "(SayText \"" + say + "\")"])
         except Exception as e:
-            print e
+            rospy.logerr('Festival subprocess call failed.')
         finally:
-            sl = r.random()*3.0
-            print "Not arrived yet - sleeping for %s" % sl        
-            time.sleep(sl)
-    arrived = False
-    spiel = True
-    stop_all = False
+            self.speaking = False
 
-def done(message):
-    global spiel, stop_all
-    if message.data == "no":
-        spiel = True
-        arrived = False
-        stop_all = False
-    else:
-        spiel = False
-        stop_all = True
+    def run(self):
+        while not rospy.is_shutdown():
+            self.sleeper.sleep()
 
-def goal_reached(message):
-    global place, arrived, spiel, speaking
-    print "Arrived: %s" % arrived
-    if 'Goal reached' in message.data and not arrived:
-        arrive = ['We have reached %s.', 'Well, here we are . . . %s', 'Welcome to the %s . . . ', 'This is the %s . . . ']
-        msg = r.choice(arrive) % place
-        while speaking or spiel:
-            pass
-        festival(msg)
-        arrived = True
-        print "Yes Arrived: %s" % arrived
+    # def nav_spiel():
+    #     rospy.init_node('nav_spiel', anonymous=True)
+    #     rospy.Subscriber("/spiel", String, callback)
+    #     rospy.Subscriber("/done", String, done)
+    #     rospy.Subscriber("/goal_reached", String, goal_reached)
+    #     rospy.Subscriber("/move_base/status", GoalStatusArray, goal_reached_action)
+    #     # spin() simply keeps python from exiting until this node is stopped
+    #     rospy.spin()
 
-def goal_reached_action(message):
-    global place, arrived, spiel, speaking
-    print "Arrived: %s" % arrived
-    if 'Goal reached' in message.status_list[-1].text and not arrived:
-        arrive = ['We have reached %s.', 'Well, here we are . . . %s', 'Welcome to the %s . . . ', 'This is the %s . . . ']
-        msg = r.choice(arrive) % place
-        while speaking or spiel:
-            pass
-        festival(msg)
-        arrived = True
-        print "Yes Arrived: %s" % arrived
-
-
-def festival(say):
-    global speaking
-    speaking = True
-    subprocess.call(["festival", "--batch", "(SayText \"" + say + "\")"])
-    speaking = False 
-
-def nav_spiel():
+def main(args):
     rospy.init_node('nav_spiel', anonymous=True)
-    rospy.Subscriber("/spiel", String, callback)
-    rospy.Subscriber("/done", String, done)
-    rospy.Subscriber("/goal_reached", String, goal_reached)
-    rospy.Subscriber("/move_base/status", GoalStatusArray, goal_reached_action)
-    # spin() simply keeps python from exiting until this node is stopped
+    ns = NavSpiel()
+    ns.start()
     rospy.spin()
-   
+
 if __name__ == '__main__':
-    try:
-        nav_spiel()
-    except rospy.ROSInterruptException:
-        pass
+    main(sys.argv)
