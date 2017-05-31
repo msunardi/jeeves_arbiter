27f5fbed4948fbd65dfab33af851216f183d166c
Mathias Sunardi <misunardi@hotmail.com>
2017-05-31 02:09:12 -0700

    Add handling of info, code cleanup

--- src/arbiter_server.py
+++ src/arbiter_server.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python
-
+'''
+    Arbiter
+    Handles interaction from Jeeves Web UI
+    Currently handles tour info and actions
+    Invoke by calling 'arbiter_server' Service with arguments:
+    - itype: interaction type - [nav, tour, info, play, or other]
+    - value: keyword for information
+    - description: additional information/description
+'''
 from arbiter.srv import *
 import rospy
 from std_msgs.msg import *
@@ -7,7 +15,6 @@ from basics.srv import *
 import random as r
 import subprocess
 
-# nav_say = rospy.Publisher('/jeeves_speech/speech_synthesis', String, queue_size=10)
 spiel = rospy.Publisher('/spiel', String, queue_size=10)
 
 prompt = ['Excellent choice .', 'Very good .', 'Certainly .', 'Of course .', 'Right . ', 'Naturally .', 'Indeed .', 'Obviously .', 'Indubitably .', 'Why, of course .', 'Marvelous .', 'Magnificent .']
@@ -23,33 +30,23 @@ def handler(request):
     response = "I don't know what you're asking"
 
     # Make decisions based on type
-    if itype in ['nav', 'info']:
+    if itype == 'nav':
         response = "Navigation request to: %s" % val
         p = r.choice(prompt)
         msg = r.choice(['Ah, ... ', '']) +  description + ', ...  ' + p
-        # nav_say.publish(msg)
         festival(msg)
-
         spiel.publish('%s:%s' % (itype, val))
 
-        #rospy.wait_for_service('waypoint')
-        #rospy.loginfo("Attempting nav: %s" % val)
-        #print response
-        #try:
-        #    wp = rospy.ServiceProxy('waypoint', Waypoint)
-        #    resp = wp(val)
-        #    return resp.output
-        #except rospy.ServiceException, e:
-        #    rospy.logerr("Unable to call service 'waypoint'.")
-        #    print "Unable to call service. %s" % e
     elif itype == 'tour':
         response = "Tour request"
     elif itype == 'info':
         response = "Requesting info for: %s" % val
+        p = r.choice(prompt)
+        msg = r.choice(['Ah, ... ', 'Ah, yes,', '']) +  description + ', ...  ' + p
+        festival(msg)
+        spiel.publish('%s:%s' % (itype, val))
     elif itype == 'play':
         response = "Requesting play behavior"
-
-    # nav_say.publish(response)
 
     return InteractResponse(response)
     # return InteractResponse("Got foo -- itype: %s - value: %s" % (request.itype, request.value))
